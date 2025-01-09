ecd51e44f2ffccc550e0d9662cb6de6ecb9a298f
InkyChew <inky11308@gmail.com>
2025-01-09 21:20:46 +0800

    whole bg color

--- src/app/pomodoro/pomodoro.component.ts
+++ src/app/pomodoro/pomodoro.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { pageTransition, tabTransition } from './transitions/route-transition';
 import { filter } from 'rxjs';
+import { SettingService } from './services/setting.service';
 
 @Component({
   selector: 'app-pomodoro',
@@ -15,14 +16,20 @@ export class PomodoroComponent {
 
   page: number = 0;
   tab: number = 0;
+  color: string = "";
 
-  constructor(private router: Router, private _route: ActivatedRoute) {
+  constructor(private router: Router,
+    private _route: ActivatedRoute,
+    private _settingService: SettingService) {
 
     this.router.events.pipe(
       filter(event => event instanceof NavigationEnd)
     ).subscribe(() => {
       this.page = +this._route.firstChild?.snapshot.data['page'];
       this.tab = +this._route.firstChild?.snapshot.params['stage'];
+      if(this.tab) {
+        this._settingService.getClockSetting(this.tab).subscribe(res => this.color = res.color);
+      }
     });
   }
 }
